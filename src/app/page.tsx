7feadce51e7e67e1f81b86e34dedcafa080c2dd8
vijayreddy <vijay1989reddy@gmail.com>
2025-05-25 09:54:29 +0530

    01:Added shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h2 className="text-3xl font-semibold tracking-tight text-balance text-green-500 sm:text-4xl">
-      Boost your productivity. Start using our app today.
-    </h2>
+    <div>
+      <h2 className="text-3xl font-semibold tracking-tight text-balance text-green-500 sm:text-4xl">
+        Boost your productivity. Start using our app today.
+      </h2>
+      <Button variant="cwa" className="rounded-none">
+        Click Me!
+      </Button>
+    </div>
   );
 }
